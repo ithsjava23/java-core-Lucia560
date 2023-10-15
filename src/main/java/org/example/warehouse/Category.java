8d1754d5e5560ea480caa7b5ef04419f1e808e18
Lucia Predescu <lucia.predescu@iths.se>
2023-10-15 17:55:36 +0200

    After clean up and creating record

--- src/main/java/org/example/warehouse/Category.java
+++ src/main/java/org/example/warehouse/Category.java
@@ -1,28 +1,39 @@
 package org.example.warehouse;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class Category {
     private String name;
-    List<Category> categoryList = new ArrayList<>();
+
     private Category(String name) {
         this.name = name;
     }
-    public List<Category> getCategoryList() {
-        return categoryList;
+
+    public String getName() {
+        return name;
     }
 
-    public void setCategoryList(List<Category> categoryList) {
-        this.categoryList = categoryList;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public static Category of(String name) {
         return new Category(name);
     }
-    public boolean getName() {
+    public boolean sameName(String newName) {
+        return Objects.equals(newName, getName());
+    }
+    public void checkFirstLetter(String n){
+
+
+    }
+    public void nullName(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("Category name can't be null");
+        }
     }
 
 
+    public record ProductRecord(String s){
+    }
 }
-public record ProductRecord()
--- src/main/java/org/example/warehouse/Warehouse.java
+++ src/main/java/org/example/warehouse/Warehouse.java
@@ -5,18 +5,30 @@ import java.util.UUID;
 
 public class Warehouse {
 
+
+
+
+
     public static Warehouse getInstance(String justAName) {
+        return null;
     }
 
     public boolean isEmpty() {
+        return false;
     }
 
     public Object addProduct(UUID uuid, String test, Category test1, Object o) {
+        return o;
     }
 
     public boolean getProductsBy(Category meat) {
+        return false;
     }
 
     public void updateProductPrice(UUID uuid, BigDecimal bigDecimal) {
+    }
+
+
+    public boolean getProducts() {
     }
 }
